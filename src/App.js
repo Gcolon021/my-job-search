1e3e404e902cb4b3f87dc800f98e013ef6e27cd9
George Colon <gcolon021@gmail.com>
2020-06-08 17:04:02 -0400

    migrating d3 to useEffect

--- src/App.js
+++ src/App.js
@@ -6,5 +6,3 @@ function App() {
 }
 
 export default App;
-
-// Spreadsheet key 2PACX-1vQASiB8n6uvnqlvkkU8VBQgBVW8hnpTnI_mf4hG98ujGMZVlx-IY7-qagFO5rZCbd4FHjxNRFKy9Tz4
--- src/draw.js
+++ src/draw.js
@@ -11,19 +11,45 @@ const Draw = () => {
 
   console.log(response);
 
+  const d3Ref = React.useRef();
+
   var height = 500;
   var width = 500;
   var radius = Math.min(width, height) / 2;
   var color = d3.scaleOrdinal(d3.schemeCategory10);
 
-  var svg = d3
-    .select("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .append("g")
+  // translates the coord system of g
+  var g = d3
+    .select(d3Ref.current) // creates <svg></svg>
+    .append("svg")
+    .attr("width", width) // sets width of svg
+    .attr("height", height) // set height of svg
+    .append("g") // creates a <g> ele inside svg 
     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
+  // defines the partition sizes in sunburst shape
   var partition = d3.partition().size([2 * Math.PI, radius]);
+
+  // uses root not and gives a size value to all of the objs
+  var root = d3.hierarchy(response).sum(function (d) { return d.size })
+
+
+  partition(root);
+  var arc = d3.arc()  // <-- 2
+    .startAngle(function (d) { return d.x0 })
+    .endAngle(function (d) { return d.x1 })
+    .innerRadius(function (d) { return d.y0 })
+    .outerRadius(function (d) { return d.y1 });
+
+  g.selectAll('g')
+    .data(root.descendants())
+    .enter().append('g').attr("class", "node")
+    .append('path')
+    .attr('display', function (d) { return d.depth ? null : "none"; })
+    .attr('d', arc)
+    .style('stroke', '#fff')
+    .style("fill", function (d) { return color((d.children ? d : d.parent).data.name) });
+
 
   return <div></div>;
 };
